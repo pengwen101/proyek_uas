b8a2b3f17c996d90f0b1af9d4a5adff31677d893
HelenaSusan <c14220149@john.petra.ac.id>
2023-12-20 09:28:32 +0700

    update h

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-        <link rel="icon" href="image/bnm logo.jpg" type="./image/jpg">
+        <link rel="icon" href="image/bnm_logo.jpg" type="./image/jpg">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
